18a1ba9686f1d445732adef8d60393f0a32fb503
playvora <longliator.dev@gmail.com>
2024-08-27 18:56:02 +0700

    Delete public/protectgui.lua

--- public/protectgui.lua
+++
@@ -1,8 +0,0 @@
-function protectgui(gui)
-    local parent = game.CoreGui.RobloxGui or game.Players.LocalPlayer.PlayerGui
-    gui.Parent = parent
-    gui.AncestryChanged:Connect(function() gui.Parent = parent end)
-    gui:GetPropertyChangedSignal("Position"):Connect(function() gui.Position = UDim2.new(0, gui.Position.X.Offset, 0, gui.Position.Y.Offset) end)
-    gui:GetPropertyChangedSignal("Size"):Connect(function() gui.Size = UDim2.new(0, gui.Size.X.Offset, 0, gui.Size.Y.Offset) end)
-    gui:GetPropertyChangedSignal("Visible"):Connect(function() gui.Visible = true end)
-end
