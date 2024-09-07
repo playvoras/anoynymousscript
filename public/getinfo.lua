33fd06ff3870ede48e99fa63d060c3a5ed0a29d8
playvora <longliator.dev@gmail.com>
2024-09-06 22:34:41 -0700

    Update getinfo.lua

--- public/getinfo.lua
+++ public/getinfo.lua
@@ -2,7 +2,7 @@ local debug = table.clone(debug)
 
 debug.getinfo = function(levelorfunc, options)
     local info = {}
-    local function fetchinfo(opt, key, convert)
+    local function infos(opt, key, convert)
         if string.find(options, opt) then
             local value = debug.info(levelorfunc, opt)
             if value ~= nil then
@@ -10,16 +10,17 @@ debug.getinfo = function(levelorfunc, options)
             end
         end
     end
-    fetchinfo("S", "source")
-    fetchinfo("s", "short_src", function(s) return s:sub(1, 60) end)
-    fetchinfo("w", "what")
-    fetchinfo("l", "currentline", tonumber)
-    fetchinfo("n", "name")
-    fetchinfo("N", "namewhat")
-    fetchinfo("f", "func")
-    fetchinfo("v", "isvararg", function(v) return v == "V" end)
-    fetchinfo("p", "nparams", tonumber)
-    fetchinfo("u", "nups", tonumber)
+    infos("S", "source")
+    infos("s", "short_src", function(s) return s:sub(1, 60) end)
+    infos("w", "what")
+    infos("l", "currentline", tonumber)
+    infos("n", "name")
+    infos("N", "namewhat")
+    infos("f", "func")
+    infos("v", "isvararg", function(v) return v == "V" end)
+    infos("p", "nparams", tonumber)
+    infos("u", "nups", tonumber)
+
     if string.find(options, "L") then
         info.activelines = {}
         local i = 1
@@ -30,18 +31,22 @@ debug.getinfo = function(levelorfunc, options)
             i = i + 1
         end
     end
+
     if string.find(options, "t") then
         info.istailcall = debug.info(levelorfunc, "t") == "T"
     end
+
     if string.find(options, "e") then
         local env = debug.info(levelorfunc, "e")
         if env then
             info.funcenv = env
         end
     end
+
     if type(levelorfunc) == "number" then
-        fetchinfo("L", "linedefined", tonumber)
-        fetchinfo("l", "lastlinedefined", tonumber)
+        infos("L", "linedefined", tonumber)
+        infos("l", "lastlinedefined", tonumber)
     end
+
     return info
 end
